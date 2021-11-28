a6c5bbddf869dffd74c69d8a1407cc0657d08ab4
davydau <davidov_dmitri_96@mail.ru>
2021-11-28 16:31:52 +0300

    feat: implemente task data types

---
+++ Conditional Statements and Data Types/Practical-task.js
@@ -0,0 +1,27 @@
+// 1. Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
+
+const stringBoolean = 'dima' + true;
+console.log(stringBoolean);
+
+const stringNumber = "dima" + 100;
+console.log(stringNumber);
+
+
+if (1 > 2) {
+    console.log('1 > 2') 
+} else {
+    console.log('2 > 1')
+}
+
+const array = [1,2,34,5];
+
+for (item of array) {
+    console.log(item)
+}
+
+
+
+
+// 2.Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+
+
