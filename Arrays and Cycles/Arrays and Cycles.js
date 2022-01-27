b97bb1cf4aa8e3935d01958d0a60cc79520a7a3b
davydau <davidov_dmitri_96@mail.ru>
2022-01-27 12:33:28 +0300

    Arrays-and-Cycles

--- Arrays and Cycles/Arrays and Cycles.js
+++ Arrays and Cycles/Arrays and Cycles.js
@@ -1,7 +1,7 @@
 // 5 Arrays and Cycles
 // 5.1 Дан массив состоящий из названий фильмов, выполните перебор массива с выводом в консоль названия каждого фильма
 
-['криминальное чтиво', 'омерзительная восьмёрка', 'бойцовский клуб'].forEach(alert);
+['криминальное чтиво', 'омерзительная восьмёрка', 'бойцовский клуб'].forEach(console.log);
 // 5.2 Дан массив производителей автомобилей, преобразовать массив в строку и обратно в массив 
 
 let arr = ['ford', 'mazda', 'honda']
@@ -11,14 +11,17 @@ let arr1 = str.split('; ');
 alert(arr1);
 // 5.3 Дан массив имен ваших знакомых, добавить к каждому элементу массива слова hello
 
-let arr = ['dima', 'dasha', 'lena', 'denis']
+let arr = ["dima", "dasha", "lena", "denis"];
+let newArr = arr.map(function (N) {
+    return N + " hello";
+});
 
-for (let i = 0; i<4; i++)
-alert(arr[i] + ' hello');
+alert(newArr);
+
 // 5.4 Преобразовать числовой массив в Boolean
 
-let arr = ['1', '2', '3', '4'];
-alert(Array.isArray(type));
+let arr = [1, 2, 0, 4].map(Boolean);
+console.log(arr);
 // 5.5 Отсортировать массив [1,6,7,8,3,4,5,6] по убыванию
 
 function compareNumeric(a, b) {
@@ -27,60 +30,60 @@ function compareNumeric(a, b) {
   if (a > b) return -1;
 }
 
-let arr = [1,6,7,8,3,4,5,6]
+let arr = [1, 6, 7, 8, 3, 4, 5, 6];
 
 arr.sort(compareNumeric);
 
-alert(arr);
+console.log(arr);
+
 // 5.6 Отфильтровать массив [1,6,7,8,3,4,5,6] по значению >3
 
-function isBig(numb){
-  return numb >3;
+function isBig(numb) {
+  return numb > 3;
 }
 
-let filtered = [1,6,7,8,3,4,5,6].filter(isBig);
-alert(filtered);
-// 5.7 Написать функцию, которая принимает два параметра - массив и число и выводит индекс элемента массива равный числ
+let filtered = [1, 6, 7, 8, 3, 4, 5, 6].filter(isBig);
+console.log(filtered);
+// 5.7 Написать функцию, которая принимает два параметра - массив и число и выводит индекс элемента массива равный числу
 
-function getElementIndex(array, element){
-    return array.indexOf(element);
- }
+function getElementIndex(array, element) {
+  return array.indexOf(element);
+}
 // 5.8 Реализовать цикл, который будет выводить число а, пока оно не станет меньше 10
 
-if (a > 10){
-  alert(i);
-} else {
-  continue;
+function C(a) {
+  while (a > 10) {
+      console.log(a);
+      a--;
+  }
 }
+
 // 5.9 Реализовать цикл, который выводит в консоль простые числа
 
-function c(n){
-  C:
-
-for (i = 1; i <= n; i++){
-
-for (j = 2; j < i; j++){
-
-if (i % j == 0){
-
-continue C;
-}} 
-console.log(i)}
+function c(n) {
+  C: for (i = 1; i <= n; i++) {
+      for (j = 2; j < i; j++) {
+          if (i % j == 0) {
+              continue C;
+          }
+      }
+      console.log(i);
+  }
 }
+
  C(7)
 // 5.10 Реализовать цикл, который выводит в консоль нечетные числа
 
-function c(n){
-  C:
-
-for (i = 1; i <= n; i++){
-  let j = 2;
-  if (i % j == 0){
-    continue C;
-  } else {
-    console.log (i)
-  }
+function c(n) {
+  C: for (i = 1; i <= n; i++) {
+      let j = 2;
+      if (i % j == 0) {
+          continue C;
+      } else {
+          console.log(i);
+      }
   }
 }
+
 
  C(9)
