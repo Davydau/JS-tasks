3749ced86695b9f16da013584ed844774d89c4e5
davydau <davidov_dmitri_96@mail.ru>
2022-01-28 14:15:29 +0300

    Prototype-and-Inheritance,-Classes,-Constructor

--- Prototype and Inheritance, Classes, Constructor/Practical-task.js
+++ Prototype and Inheritance, Classes, Constructor/Practical-task.js
@@ -1,14 +1,38 @@
 // 6 Prototype and Inheritance, Classes, Constructor
 // Создать 2 объекта: animal и cat, в объект animal добавить свойство move, объект cat должен наследовать свойство move,
 //  реализовать и через прототипы и через классы.
+// 6.1
+
 let animal = {
-    move: true
- };
+    move: true,
+};
 
- let cat = {
-   
- };
+let cat = {};
 
- cat.__proto__ = animal;
+cat.__proto__ = animal;
 
- alert(cat.move);
+console.log(cat.move);
+
+//6.2
+class animal {
+    constructor(name) {
+        this.move = true;
+    }
+    moving(move) {
+        this.move = true;
+        console.log("двигается.");
+    }
+    dontmoving(move) {
+        this.move = false;
+        console.log("не двигается.");
+    }
+}
+class Cat extends animal {
+    sleep() {
+        console.log("спит...");
+    }
+}
+let cat = new Cat();
+cat.moving();
+cat.dontmoving();
+cat.sleep();
