416ed23764bc2680905fe24a41f2d874a0e280b2
davydau <davidov_dmitri_96@mail.ru>
2022-01-30 10:38:48 +0300

    Objects-and-Functions

--- Objects and Functions/Practical-task.js
+++ Objects and Functions/Practical-task.js
@@ -6,11 +6,7 @@
 // -        В объект car добавить свойство power, которое является функцией и выводит в консоль мощность двигателя
 let car = {
   colore: "чёрный",
-<<<<<<< HEAD
-  power: "мощность двигателя", 
-=======
   power: "мощность двигателя",
->>>>>>> Arrays-and-Cycles
 };
 car.colore = "зелёный";
 console.log(car);
@@ -19,93 +15,68 @@ console.log(car);
 
 
 function calcSum(apple, pear) {
-<<<<<<< HEAD
-if (apple + pear > 1) {
   return apple + pear;
-} else {
-  return console.log('такое возможно?');
 }
-=======
+
+function calcSum(apple, pear) {
   if (apple + pear > 1) {
       return apple + pear;
   } else {
-      return console.log("такое возможно?");
+      return confirm("такое возможно?");
   }
->>>>>>> Arrays-and-Cycles
+}
+
+let apple = prompt("Сколько яблок привезли?", 1);
+let pear = prompt("Сколько груш привезли?", 1);
+
+if (calcSum(apple, pear)) {
+  console.log(+apple + +pear);
+} else {
+  console.log("такое возможно?");
 }
 
 //-        В терминале оплаты сохранено ваше имя, напишите функцию для определения имени в 
 // терминале(если вы ввели ваше имя, то привет + имя, если нет, то нет такого имени)
 
 user = {
-name: "John",
+  name: "John",
 };
 
 function chekName(key) {
-<<<<<<< HEAD
 if (user.name === key) {
-return true;
-} else {
-  return false;
-}
-=======
-  if (user.name === key) {
-      return true;
+  return true;
   } else {
-      return false;
+    return false;
   }
->>>>>>> Arrays-and-Cycles
 }
 
 let key = prompt("введите ваше имя", "");
 
-<<<<<<< HEAD
 
 if ( chekName(key) ) {
-console.log('Привет ' + key);
+  console.log('Привет ' + key);
 } else {
-console.log('нет такого имени');
-=======
-if (chekName(key)) {
-  console.log("Привет " + key);
-} else {
-  console.log("нет такого имени");
->>>>>>> Arrays-and-Cycles
+  console.log('нет такого имени');
 }
 
 //-        Напишите функцию вычисления типа аргумента и вывод типа в консоль
 
-<<<<<<< HEAD
-function getArgType(arg){
-const type = typeof arg;
-console.log(type);
-=======
+
 function getArgType(arg) {
   const type = typeof arg;
   console.log(type);
->>>>>>> Arrays-and-Cycles
 }
 
 
 //-        Напишите функцию, которая определяет является ли число простым или нет
 
 function showPrime(num, div = 2) {
-<<<<<<< HEAD
-  
- 
-  if(num <= div ) return 'составное число'; 
-   
-  if(num % 2 === 0) return 'составное число'  
-  
-  return 'простое число'; 
- 
-=======
+
   if (num <= div) return "составное число";
 
   if (num % 2 === 0) return "составное число";
 
   return "простое число";
 
->>>>>>> Arrays-and-Cycles
   return showPrime(num);
 }
