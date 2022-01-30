75e20667a54030dc6adf27718631ad001acb283e
davydau <davidov_dmitri_96@mail.ru>
2022-01-30 10:41:15 +0300

    chenges String,-RegExp,-DataTime

--- String, RegExp, DataTime/Practical-task.js
+++ String, RegExp, DataTime/Practical-task.js
@@ -3,17 +3,17 @@
 
 let str = "ahb acb aeb aeeb adcb axeb";
    let resalt = str.match(/a.b/g);
-   alert(resalt);
+   console.log(resalt);
 // 7.2 Дана строка '2+3 223 2223'. Напишите регулярку, которая найдет строку 2+3, не захватив остальные
 
 let str = "2+3 223 2223'";
    let resalt = str.match(/2\+3/g);
-   alert(resalt);
+   console.log(resalt);
 // 7.3 Получить день, месяц и год текущей даты и по отдельности вывести в консоль
 
  var date = new Date();
  
 
-   alert(date.getDay());
-   alert(date.getMonth());
-   alert(date.getFullYear());
+ console.log(date.getDay());
+ console.log(date.getMonth());
+ console.log(date.getFullYear());
