9310e3144fc9e2c98f43ad1c7e53da6314745ab6
davydau <davidov_dmitri_96@mail.ru>
2022-02-12 10:28:41 +0300

    new chenges

--- Exception handling/Practical-task.js
+++ Exception handling/Practical-task.js
@@ -2,20 +2,20 @@
 
 // 8.1 В try catch конструкцию завернуть код: console.log(a), let a = 3. И вывести ошибку – ‘let перед использованием нужно объявить’.
 try {
-   console.log(a);
-   let a = 3;
+    console.log(a);
+    let a = 3;
 } catch (err) {
-   console.log("‘let перед использованием нужно объявить’");
+    console.log('‘let перед использованием нужно объявить’');
 }
- // 8.2 При выполнении 1/0  выводить ошибку 'на ноль делить нельзя'
- function culcDivision(a, b) {
-   try {
-       if (b == 0) {
-           throw new Error("на ноль делить нельзя");
-       } else {
-           console.log(a / b);
-       }
-   } catch (e) {
-       console.log("на ноль делить нельзя");
-   }
+// 8.2 При выполнении 1/0  выводить ошибку 'на ноль делить нельзя'
+function culcDivision(a, b) {
+    try {
+        if (b == 0) {
+            throw new Error('на ноль делить нельзя');
+        } else {
+            console.log(a / b);
+        }
+    } catch (e) {
+        console.log('на ноль делить нельзя');
+    }
 }
