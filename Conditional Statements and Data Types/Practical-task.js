c6b2d2c322315d4df14a414054a0b5053506646f
davydau <davidov_dmitri_96@mail.ru>
2022-02-12 10:02:07 +0300

    new changes

--- Conditional Statements and Data Types/Practical-task.js
+++ Conditional Statements and Data Types/Practical-task.js
@@ -16,39 +16,37 @@ const stringBoolean2 = 'dima' * true;
 console.log(stringBoolean2);
 
 const stringNomber2 = 'dima' * 2;
-console.log(stringNomber2);
+console.log(stringNumber2);
 
 const numberBoolean2 = 2 * true;
 console.log(numberBoolean2);
 
 // 3.3 Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
 
-const stringBoolean2olean3 = 'dima' / true;
-console.log(stringBoolean2olean3);
+const stringBoolean2boolean3 = 'dima' / true;
+console.log(stringBoolean2boolean3);
 
-const stringNomber3 = 'dima' / 2;
-console.log(stringNomber3);
+const stringNumber3 = 'dima' / 2;
+console.log(stringNumber3);
 
 const numberBoolean3 = 2 / true;
 console.log(numberBoolean3);
 
 //3.4 Выполнить явное преобразование(number, string, boolean)
 
-let str = "123";
-let num = Number(str); 
-console.log(typeof num); 
-
+let str = '123';
+let num = Number(str);
+console.log(typeof num);
 
 let value = true;
 value = String(value);
 console.log(typeof value);
 
-
-let bool = (Boolean("Hi"));
+let bool = Boolean('Hi');
 console.log(bool);
 
-let bool1 = (Boolean(""));
+let bool1 = Boolean('');
 console.log(bool1);
 
-let bool2 = (Boolean("0"));
+let bool2 = Boolean('0');
 console.log(bool2);
