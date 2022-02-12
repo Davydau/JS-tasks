f638bfc224582150b238303e56a143cc42f81d7d
davydau <davidov_dmitri_96@mail.ru>
2022-02-12 13:25:46 +0300

    some new chenges

--- Conditional Statements and Data Types/Practical-task.js
+++ Conditional Statements and Data Types/Practical-task.js
@@ -1,5 +1,5 @@
 //3 Conditional Statements and Data Types
-// 3.1 Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
+// 3.1 Выполнить сложение различных типов(string+boolean, string+number, number+boolean).
 
 const stringBoolean1 = 'dima' + true;
 console.log(stringBoolean1);
@@ -10,7 +10,7 @@ console.log(stringNumber1);
 const numberBoolean1 = 100 + true;
 console.log(numberBoolean1);
 
-// 3.2. Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+// 3.2. Выполнить умножение различных типов(string * boolean, string * number, number * boolean).
 
 const stringBoolean2 = 'dima' * true;
 console.log(stringBoolean2);
@@ -21,7 +21,7 @@ console.log(stringNumber2);
 const numberBoolean2 = 2 * true;
 console.log(numberBoolean2);
 
-// 3.3 Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
+// 3.3 Выполнить деление различных типов(string/boolean, string/number, number/Boolean).
 
 const stringBoolean2boolean3 = 'dima' / true;
 console.log(stringBoolean2boolean3);
@@ -32,7 +32,7 @@ console.log(stringNumber3);
 const numberBoolean3 = 2 / true;
 console.log(numberBoolean3);
 
-//3.4 Выполнить явное преобразование(number, string, boolean)
+//3.4 Выполнить явное преобразование(number, string, boolean).
 
 let str = '123';
 let num = Number(str);
