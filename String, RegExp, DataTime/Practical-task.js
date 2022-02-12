47a93088b41b4fc1df7251e2b836388965751db1
davydau <davidov_dmitri_96@mail.ru>
2022-02-12 10:26:53 +0300

    new chenges

--- String, RegExp, DataTime/Practical-task.js
+++ String, RegExp, DataTime/Practical-task.js
@@ -1,19 +1,18 @@
-// 7.1 Дана строка 'ahb acb aeb aeeb adcb axeb'. Напишите регулярное выражение, 
-// которое найдет строки ahb, acb, aeb по шаблону: буква 'a', любой символ, буква 'b' 
+// 7.1 Дана строка 'ahb acb aeb aeeb adcb axeb'. Напишите регулярное выражение,
+// которое найдет строки ahb, acb, aeb по шаблону: буква 'a', любой символ, буква 'b'
 
-let str = "ahb acb aeb aeeb adcb axeb";
-   let resalt = str.match(/a.b/g);
-   console.log(resalt);
+let str = 'ahb acb aeb aeeb adcb axeb';
+let result = str.match(/a.b/g);
+console.log(result);
 // 7.2 Дана строка '2+3 223 2223'. Напишите регулярку, которая найдет строку 2+3, не захватив остальные
 
 let str = "2+3 223 2223'";
-   let resalt = str.match(/2\+3/g);
-   console.log(resalt);
+let result = str.match(/2\+3/g);
+console.log(result);
 // 7.3 Получить день, месяц и год текущей даты и по отдельности вывести в консоль
 
- var date = new Date();
- 
+var date = new Date();
 
- console.log(date.getDay());
- console.log(date.getMonth());
- console.log(date.getFullYear());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getFullYear());
