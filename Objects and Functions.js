0a54ef0cdd62b507e76884b528e9edb829b41d95
davydau <davidov_dmitri_96@mail.ru>
2022-02-15 14:37:50 +0300

    error correction

--- Objects and Functions.js
+++ Objects and Functions.js
@@ -6,19 +6,18 @@
 // -        В объект car добавить свойство power, которое является функцией и выводит в консоль мощность двигателя
 let car = {
     color: 'чёрный',
-    power: 'мощность двигателя',
 };
 car.color = 'зелёный';
-console.log(car);
+car.power = function ShowPower() {
+    console.log('мощность двигателя');
+};
+car.power();
+console.log(car.color);
 
 // -         На склад принимают груши и яблоки, напишите функцию, которая возвращает результат сложения количества принятых груш и яблок
 
 function calcSum(apple, pear) {
-    if (apple + pear > 1) {
-        return apple + pear;
-    } else {
-        return console.log('такое возможно?');
-    }
+    return apple + pear;
 }
 
 //-        В терминале оплаты сохранено ваше имя, напишите функцию для определения имени в
@@ -29,36 +28,29 @@ user = {
 };
 
 function checkName(key) {
+    key = prompt('введите ваше имя', 'John');
     if (user.name === key) {
-        return true;
+        console.log('Привет ' + key);
     } else {
-        return false;
+        console.log('нет такого имени');
     }
 }
-
-let key = prompt('введите ваше имя', '');
-
-if (checkName(key)) {
-    console.log('Привет ' + key);
-} else {
-    console.log('нет такого имени');
-}
+checkName();
 
 //-        Напишите функцию вычисления типа аргумента и вывод типа в консоль
 
-function getArgType(arg) {
-    const type = typeof arg;
+function getArgType(valueToCheckType) {
+    const type = typeof valueToCheckType;
     console.log(type);
 }
 
 //-        Напишите функцию, которая определяет является ли число простым или нет
 
-function showPrime(num, div = 2) {
-    if (num <= div) return 'составное число';
-
-    if (num % 2 === 0) return 'составное число';
-
-    return 'простое число';
-
-    return showPrime(num);
+function checkPrime(num) {
+    for (let j = 2; j < num; j++) {
+        if (num % j == 0) {
+            return 'число составное';
+        }
+    }
+    return 'число простое';
 }
